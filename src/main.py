98e8bcc59576bc8d9c116e4eaff15a80c7e4ccd0
maid-er <maideruranga26@gmail.com>
2024-05-09 21:13:14 +0200

    Modify main to execute each instance more than once

--- src/main.py
+++ src/main.py
@@ -28,31 +28,31 @@ def executeDir():
         ficheros = [file.name for file in files if file.is_file() and file.name.endswith(".txt")]
 
     with open('resultados.csv', "w") as results:
-        # results.write(
-        #     'instance,RL_01,RL_03,RL_05,RL_07,RL_09,BR_T,BR_G_01,BR_G_03,BR_G_05,BR_G_07,BR_G_09,\n'
-        #     )
-        results.write(
-            'instance,BR_T_MaxSum,BR_T_MaxMin,\n'
-        )
-        for f in ficheros:
-            path = os.path.join(dir, f)
-            logging.info('Solving instance %s:', f)
-            inst = instance.readInstance(path)
-            results.write(f + ',')
-            for method_config in config:
-                start = datetime.datetime.now()
-                sol = grasp.execute(inst, 100, method_config)
-                elapsed = datetime.datetime.now() - start
-                secs = round(elapsed.total_seconds(), 2)
-                logging.info('MaxSum objective function value for the best result: %s',
-                             sol['of_MaxSum'])
-                logging.info('MaxMin objective function value for the best result: %s',
-                             sol['of_MaxMin'])
-                logging.info('Execution time: %s', secs)
-                results.write(str(round(sol['of_MaxSum'], 2)) + ',')
-                results.write(str(round(sol['of_MaxMin'], 2)) + ',')
 
-            results.write('\n')
+        # results.write(
+        #     'instance,BR_T_MaxSum,BR_T_MaxMin,\n'
+        # )
+
+        for f in ficheros:
+            for _ in range(8):
+                path = os.path.join(dir, f)
+                logging.info('Solving instance %s:', f)
+                inst = instance.readInstance(path)
+                results.write(f + ',')
+                for method_config in config:
+                    start = datetime.datetime.now()
+                    sol = grasp.execute(inst, 100, method_config)
+                    elapsed = datetime.datetime.now() - start
+                    secs = round(elapsed.total_seconds(), 2)
+                    logging.info('MaxSum objective function value for the best result: %s',
+                                 sol['of_MaxSum'])
+                    logging.info('MaxMin objective function value for the best result: %s',
+                                 sol['of_MaxMin'])
+                    logging.info('Execution time: %s', secs)
+                    results.write(str(round(sol['of_MaxSum'], 2)) + ',')
+                    results.write(str(round(sol['of_MaxMin'], 2)) + ',')
+
+        results.write('\n')
 
 
 if __name__ == '__main__':
