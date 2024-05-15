eb7641759154ad6335477a5d52fa618ac864d470
maid-er <maideruranga26@gmail.com>
2024-05-15 19:21:27 +0200

    Save and plot results using pandas dataframe

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 '''Main function'''
 import datetime
+import matplotlib.pyplot as plt
 import os
+import pandas as pd
 import random
 
 from structure import solution, instance
@@ -27,41 +29,43 @@ def executeDir():
     with os.scandir(dir) as files:
         ficheros = [file.name for file in files if file.is_file() and file.name.endswith(".txt")]
 
-    with open('resultados.csv', "w") as results:
+    result_table = pd.DataFrame(columns=['Solution', 'MaxSum', 'MaxMin', 'Cost', 'Capacity'])
 
-        # results.write(
-        #     'instance,BR_T_MaxSum,BR_T_MaxMin,\n'
-        # )
-
-        for f in ficheros:
-            for _ in range(8):
-                path = os.path.join(dir, f)
-                logging.info('Solving instance %s:', f)
-                inst = instance.read_USCAP_instance(path)
-                results.write(f + ',')
-                for method_config in config:
-                    start = datetime.datetime.now()
-                    sol = grasp.execute(inst, 100, method_config)
-                    elapsed = datetime.datetime.now() - start
-                    secs = round(elapsed.total_seconds(), 2)
-                    logging.info('MaxSum objective function value for the best result: %s',
-                                 sol['of_MaxSum'])
-                    logging.info('MaxMin objective function value for the best result: %s',
-                                 sol['of_MaxMin'])
-                    logging.info('Execution time: %s', secs)
-                    results.write(' - '.join([str(s+1) for s in sol['sol']]) + ',')
-                    results.write(str(round(sol['of_MaxSum'], 2)) + ',')
-                    results.write(str(round(sol['of_MaxMin'], 2)) + ',')
-
-                logging.info('Final solution:')
-                logging.info('Selected elements: %s', sol['sol'])
+    for f in ficheros:
+        for _ in range(8):
+            path = os.path.join(dir, f)
+            logging.info('Solving instance %s:', f)
+            inst = instance.read_USCAP_instance(path)
+            for method_config in config:
+                start = datetime.datetime.now()
+                sol = grasp.execute(inst, 100, method_config)
+                elapsed = datetime.datetime.now() - start
+                secs = round(elapsed.total_seconds(), 2)
                 logging.info('MaxSum objective function value for the best result: %s',
                              sol['of_MaxSum'])
                 logging.info('MaxMin objective function value for the best result: %s',
                              sol['of_MaxMin'])
-                logging.info('Cost: %s, Capacity: %s', sol['total_cost'], sol['total_capacity'])
+                logging.info('Execution time: %s', secs)
 
-                results.write('\n')
+                selected_nodes = ' - '.join([str(s+1) for s in sol['sol']])
+                result_table.loc[len(result_table)] = [selected_nodes] + [sol.get(key)
+                                                                          for key in [
+                                                                              'of_MaxSum',
+                                                                              'of_MaxMin',
+                                                                              'total_cost',
+                                                                              'total_capacity']]
+
+            logging.info('Final solution:')
+            logging.info('Selected elements: %s', sol['sol'])
+            logging.info('MaxSum objective function value for the best result: %s',
+                         sol['of_MaxSum'])
+            logging.info('MaxMin objective function value for the best result: %s',
+                         sol['of_MaxMin'])
+            logging.info('Cost: %s, Capacity: %s', sol['total_cost'], sol['total_capacity'])
+
+        result_table.to_csv(f'results_{dir}.csv', index=False)
+        result_table.plot.scatter(x='MaxSum', y='MaxMin')
+        plt.show()
 
 
 if __name__ == '__main__':
