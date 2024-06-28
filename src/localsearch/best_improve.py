9bc384f8fb8c4bab90b791b1121a7424686251c3
maid-er <maideruranga26@gmail.com>
2024-06-29 00:26:48 +0200

    Generalize best_improve local search module to apply in VND

--- src/localsearch/best_improve.py
+++ src/localsearch/best_improve.py
@@ -3,8 +3,9 @@ Auxiliar function to apply Best Improve Local Search.
 The worst selected element and best unselected element are interchanged to improve
 the initial solution.
 '''
-from structure.solution import Solution
+from itertools import combinations
 
+from structure.solution import Solution
 from utils.logger import load_logger
 
 logging = load_logger(__name__)
@@ -28,49 +29,50 @@ def improve(sol: Solution, max_iter: int = 50):
                  abs_count, count)
 
 
-def try_improvement(sol: Solution) -> bool:
+def try_improvement(sol: Solution, neighborhood: int = [1, 1]) -> bool:
     '''Attempts to improve a solution by selecting and interchanging a selected element (node)
     with an unselected element. The improvement is obtained if the sum of the distances of the
     new element to the rest of the selected nodes is higher than the distance of the previous
     selection.
 
     Args:
-      sol (Solution): contains the solution information.
+        sol (Solution): contains the solution information.
 
     Returns:
-      (bool): `True` if the improvement was successful (i.e., if the objective values are
-    improved and constraints are met with the interchange), and `False` otherwise.
+    (bool): `True` if the improvement was successful (i.e., if `ofVarSel` is less than
+    `ofVarUnsel`), and `False` otherwise.
     '''
-    (worst_selected, sel_maxsum_variability, sel_maxmin,
-     best_unselected, unsel_maxsum_variability, unsel_maxmin) = select_interchange(sol)
+    (worst_selected,
+     sel_maxsum_variability, sel_maxmin,
+     best_unselected,
+     unsel_maxsum_variability, unsel_maxmin) = select_interchange(sol, neighborhood)
 
-    if ((sel_maxsum_variability <= unsel_maxsum_variability) and (sel_maxmin <= unsel_maxmin)
-        and sol.satisfies_cost([best_unselected], [worst_selected])
-            and sol.satisfies_capacity([best_unselected], [worst_selected])):
+    if (sel_maxsum_variability <= unsel_maxsum_variability) and (sel_maxmin <= unsel_maxmin):
 
-        sol.add_to_solution(best_unselected, unsel_maxmin, unsel_maxsum_variability)
-        sol.remove_from_solution(worst_selected, sel_maxmin, sel_maxsum_variability)
+        for v in best_unselected:
+            sol.add_to_solution(v, unsel_maxmin, unsel_maxsum_variability)
+        for u in worst_selected:
+            sol.remove_from_solution(u, sel_maxmin, sel_maxsum_variability)
         return True
     return False
 
 
-def select_interchange(sol: Solution):
+def select_interchange(sol: Solution, neighborhood: list):
     '''Interchanges the worst element in solution (lowest sum of distances to the rest of the
     selected elements) with the best unselected element (highest sum of distances to the rest
     of the selected elements).
 
     Args:
-      sol (Solution): contains the solution information.
+    sol (dict): contains the solution information in three key-value pairs: 'sol' with the set
+    of selected candidates for the solution, 'of' with the objective value, and 'instance' that
+    contains the instance data, with key 'd' representing the distance matrix between all the
+    candidate nodes.
 
     Returns:
-      sel (int): worst selected element ID.
-      best_sum_sel (float): sum of distances from `sel` to the rest of the elements in solution.
-      best_min_sel (float): minimum distance from `sel` to the rest of the elements in solution.
-      unsel (int): best unselected element ID.
-      best_sum_unsel (float): sum of distances from `unsel` to the rest of the elements in
-    solution.
-      best_min_unsel (float): minimum distance from `unsel` to the rest of the elements in
-    solution.
+    sel (int): worst selected element ID.
+    bestSel (float): sum of distances from `sel` to the rest of the elements in solution.
+    unsel (int): best unselected element ID.
+    bestUnsel (float): sum of distances from `unsel` to the rest of the elements in solution.
     '''
     n = sol.instance['n']
     sel = -1
@@ -79,19 +81,24 @@ def select_interchange(sol: Solution):
     unsel = -1
     best_sum_unsel = 0
     best_min_unsel = 0
-    for v in sol.solution_set:
-        d_sum = sol.distance_sum_to_solution(v)
-        d_min = sol.minimum_distance_to_solution(v)
-        if d_sum <= best_sum_sel and d_min <= best_min_sel:
-            best_sum_sel = d_sum
-            best_min_sel = d_min
-            sel = v
-    for v in range(n):
-        if not sol.contains(v):
-            d_sum = sol.distance_sum_to_solution(v, without=sel)
-            d_min = sol.minimum_distance_to_solution(v, without=sel)
-            if d_sum >= best_sum_unsel and d_min >= best_min_unsel:
-                best_sum_unsel = d_sum
-                best_min_unsel = d_min
-                unsel = v
+    for combo in combinations(sol.solution_set, neighborhood[0]):
+        d_sum = [sol.distance_sum_to_solution(v) for v in combo]
+        d_min = [sol.minimum_distance_to_solution(v) for v in combo]
+        if sum(d_sum) <= best_sum_sel and max(d_min) <= best_min_sel:
+            best_sum_sel = sum(d_sum)
+            best_min_sel = max(d_min)
+            sel = list(combo)
+
+    for combo in combinations(range(n), neighborhood[1]):
+        if not any(sol.contains(v) for v in combo):
+            d_sum = [sol.distance_sum_to_solution(v, without=sel) for v in combo]
+            d_min = [sol.minimum_distance_to_solution(v, without=sel) for v in combo]
+            if sum(d_sum) >= best_sum_unsel and max(d_min) >= best_min_unsel \
+                and sol.satisfies_cost(combo, sel) \
+                    and sol.satisfies_capacity(combo, sel):
+
+                best_sum_unsel = sum(d_sum)
+                best_min_unsel = max(d_min)
+                unsel = list(combo)
+
     return sel, best_sum_sel, best_min_sel, unsel, best_sum_unsel, best_min_unsel
--- src/localsearch/variable_neighborhood_descent.py
+++ src/localsearch/variable_neighborhood_descent.py
@@ -6,6 +6,7 @@ the initial solution.
 import random
 from itertools import combinations
 
+from localsearch import best_improve as bs
 from structure.solution import Solution
 
 from utils.logger import load_logger
@@ -34,8 +35,7 @@ def improve(sol: Solution):
         switch = neighborhoods[nb]
         logging.info('Local searching in neighbourhood %s with switch type %s.', nb, switch)
         if LS == 'best':
-            best_improve = BestImprove()
-            improve = best_improve.try_improvement(sol, switch)
+            improve = bs.try_improvement(sol, switch)
         else:
             first_improve = FirstImprove()
             improve = first_improve.tryImprove(sol, switch)
@@ -49,81 +49,6 @@ def improve(sol: Solution):
         abs_count += 1
     logging.info('Local search stopped with %s total IT and %s IT with no improvements.',
                  abs_count, count)
-
-
-class BestImprove:
-    def try_improvement(self, sol: Solution, neighborhood: int) -> bool:
-        '''Attempts to improve a solution by selecting and interchanging a selected element (node)
-        with an unselected element. The improvement is obtained if the sum of the distances of the
-        new element to the rest of the selected nodes is higher than the distance of the previous
-        selection.
-
-        Args:
-            sol (Solution): contains the solution information.
-
-        Returns:
-        (bool): `True` if the improvement was successful (i.e., if `ofVarSel` is less than
-        `ofVarUnsel`), and `False` otherwise.
-        '''
-        (worst_selected,
-         sel_maxsum_variability, sel_maxmin,
-         best_unselected,
-         unsel_maxsum_variability, unsel_maxmin) = self.select_interchange(sol, neighborhood)
-
-        if (sel_maxsum_variability <= unsel_maxsum_variability) and (sel_maxmin <= unsel_maxmin):
-
-            for v in best_unselected:
-                sol.add_to_solution(v, unsel_maxmin, unsel_maxsum_variability)
-            for u in worst_selected:
-                sol.remove_from_solution(u, sel_maxmin, sel_maxsum_variability)
-            return True
-        return False
-
-    def select_interchange(self, sol: Solution, neighborhood: list):
-        '''Interchanges the worst element in solution (lowest sum of distances to the rest of the
-        selected elements) with the best unselected element (highest sum of distances to the rest
-        of the selected elements).
-
-        Args:
-        sol (dict): contains the solution information in three key-value pairs: 'sol' with the set
-        of selected candidates for the solution, 'of' with the objective value, and 'instance' that
-        contains the instance data, with key 'd' representing the distance matrix between all the
-        candidate nodes.
-
-        Returns:
-        sel (int): worst selected element ID.
-        bestSel (float): sum of distances from `sel` to the rest of the elements in solution.
-        unsel (int): best unselected element ID.
-        bestUnsel (float): sum of distances from `unsel` to the rest of the elements in solution.
-        '''
-        n = sol.instance['n']
-        sel = -1
-        best_sum_sel = 0x3f3f3f3f
-        best_min_sel = 0x3f3f3f3f
-        unsel = -1
-        best_sum_unsel = 0
-        best_min_unsel = 0
-        for combo in combinations(sol.solution_set, neighborhood[0]):
-            d_sum = [sol.distance_sum_to_solution(v) for v in combo]
-            d_min = [sol.minimum_distance_to_solution(v) for v in combo]
-            if sum(d_sum) <= best_sum_sel and max(d_min) <= best_min_sel:
-                best_sum_sel = sum(d_sum)
-                best_min_sel = max(d_min)
-                sel = list(combo)
-
-        for combo in combinations(range(n), neighborhood[1]):
-            if not any(sol.contains(v) for v in combo):
-                d_sum = [sol.distance_sum_to_solution(v, without=sel) for v in combo]
-                d_min = [sol.minimum_distance_to_solution(v, without=sel) for v in combo]
-                if sum(d_sum) >= best_sum_unsel and max(d_min) >= best_min_unsel \
-                    and sol.satisfies_cost(combo, sel) \
-                        and sol.satisfies_capacity(combo, sel):
-
-                    best_sum_unsel = sum(d_sum)
-                    best_min_unsel = max(d_min)
-                    unsel = list(combo)
-
-        return sel, best_sum_sel, best_min_sel, unsel, best_sum_unsel, best_min_unsel
 
 
 class FirstImprove:
