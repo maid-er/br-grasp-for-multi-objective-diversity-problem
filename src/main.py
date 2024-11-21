132e9bdb7dcfdc0db2a59ec7453a593b606fce32
maid-er <maideruranga26@gmail.com>
2024-11-21 15:25:49 +0100

    Make # of experiments configurable

--- src/main.py
+++ src/main.py
@@ -13,11 +13,11 @@ config_list = read_config('config')
 if __name__ == '__main__':
     print('Initializing diversity maximization algorithm...')
 
-    path = os.path.join('instances', 'pr_test', 'aux_sbt')
+    path = os.path.join('instances', 'pr_test', 'GKD-b_n50')
 
     for config in config_list:
         # execution.execute_instance(path, config)
-        for n in range(1):
+        for n in range(config.get('experiments')):
             execution.execute_directory(path, config)
 
         os.remove(os.path.join('temp', 'execution.txt'))
